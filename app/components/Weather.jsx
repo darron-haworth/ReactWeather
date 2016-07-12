3f5585889000748f37a4afb76fd1aaae8214f9b6
Darron Haworth <darron.haworth@firsttechfed.com>
2016-07-12 11:48:50 -0700

    Faking our api call

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -4,12 +4,25 @@ var WeatherMessage = require('WeatherMessage');
                 
 
 var Weather = React.createClass({
+    getInitialState: function() {
+        return {
+            location: 'Sacramento',
+            temp: 88
+        }
+    },
+    handleSearch: function (location) {
+        this.setState({
+            location: location,
+            temp: 99
+        });
+    },
     render: function () {
+        var {temp, location} = this.state;
         return (
             <div>
                 <h1>Get Weather</h1>
-                <WeatherForm />
-                <WeatherMessage />
+                <WeatherForm onSearch={this.handleSearch} />
+                <WeatherMessage temp={temp} location={location} />
             </div>
         );
     }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,8 +2,9 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
     render: function () {
+        var {temp, location} = this.props;
         return (
-            <h4>It is rather cool in Hades</h4>
+            <h4>It is {temp} in {location}!</h4>
         );
     }
 });
